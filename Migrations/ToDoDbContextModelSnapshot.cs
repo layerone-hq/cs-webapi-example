322a7f35757e4e5fea524ac99858912ba3904163
Bruno Ferreira <eu.brunoferreira@gmail.com>
2026-04-12 14:36:45 +0100

    chore: add .NET CI workflow and update project dependencies to version 10.0.0

--- Migrations/ToDoDbContextModelSnapshot.cs
+++ Migrations/ToDoDbContextModelSnapshot.cs
@@ -14,7 +14,7 @@ namespace webapi_demo.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "9.0.4");
+            modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
 
             modelBuilder.Entity("webapi_demo.Models.ToDoItem", b =>
                 {
